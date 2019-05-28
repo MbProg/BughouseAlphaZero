90bab56943c0daaa978a534539e25dbee3c0f0a1
Alexander Klein <KleinSchwab@gmail.com>
2019-05-29 00:54:18 +0200

    Update state generation of the boards

--- bughouse/BugHouseBoard.py
+++ bughouse/BugHouseBoard.py
@@ -10,8 +10,8 @@ class _BughouseBoardState:
 
     def restore(self, board):
         self.board_state.restore(board)
-        board._bughouse_boards.boards[board.other_board_id].pockets = copy.deepcopy(self.pockets)
-        board.pockets = copy.deepcopy(self.pockets_self)
+        board._bughouse_boards.boards[board.other_board_id].pockets = self.pockets.copy()
+        board.pockets = self.pockets_self.copy()
 
 
 class _BughouseBoard(chess.variant.CrazyhouseBoard):
@@ -43,45 +43,98 @@ class _BughouseBoard(chess.variant.CrazyhouseBoard):
 
     def to_numpy(self, flip: bool):
         #order of figurs
-        # White PAWN, KNIGHT, BISHOP, ROOK, QUEEN, KING then Black PAWN, KNIGHT, BISHOP, ROOK, QUEEN, KING
-        board_state = np.zeros((12, 8, 8))
+        # Black = 0, White = 1
+        # PAWN, KNIGHT, BISHOP, ROOK, QUEEN, KING
+
+        # Piece postions on the board and promoted pieces
+        piece_mat = np.zeros((2, 6, 8, 8))
+        promoted_mat = np.zeros((2, 8, 8))  # First White then Black
         rank = 0
         file = 0
+        # check each tile for figures and promotion
         for square in chess.SQUARES_180:
             mask = chess.BB_SQUARES[square]
-            if not self.occupied & mask:
-                pass
-            elif self.pawns & mask:
-                color = bool(self.occupied_co[chess.WHITE] & mask)
-                fig = chess.PAWN + ((not color)*6) - 1
-                board_state[fig, rank, file] = 1
-            elif self.knights & mask:
-                color = bool(self.occupied_co[chess.WHITE] & mask)
-                fig = chess.KNIGHT + ((not color)*6) - 1
-                board_state[fig, rank, file] = 1
-            elif self.bishops & mask:
-                color = bool(self.occupied_co[chess.WHITE] & mask)
-                fig = chess.BISHOP + ((not color)*6) - 1
-                board_state[fig, rank, file] = 1
-            elif self.rooks & mask:
-                color = bool(self.occupied_co[chess.WHITE] & mask)
-                fig = chess.ROOK + ((not color)*6) - 1
-                board_state[fig, rank, file] = 1
-            elif self.queens & mask:
-                color = bool(self.occupied_co[chess.WHITE] & mask)
-                fig = chess.QUEEN + ((not color)*6) - 1
-                board_state[fig, rank, file] = 1
-            elif self.kings & mask:
-                color = bool(self.occupied_co[chess.WHITE] & mask)
-                fig = chess.KING + ((not color)*6) - 1
-                board_state[fig, rank, file] = 1
+            if self.occupied & mask:
+                if self.pawns & mask:
+                    color = int(bool(self.occupied_co[chess.WHITE] & mask))
+                    fig = chess.PAWN - 1
+                    piece_mat[color, fig, rank, file] = 1
+                elif self.knights & mask:
+                    color = int(bool(self.occupied_co[chess.WHITE] & mask))
+                    fig = chess.KNIGHT - 1
+                    piece_mat[color, fig, rank, file] = 1
+                elif self.bishops & mask:
+                    color = int(bool(self.occupied_co[chess.WHITE] & mask))
+                    fig = chess.BISHOP - 1
+                    piece_mat[color, fig, rank, file] = 1
+                elif self.rooks & mask:
+                    color = int(bool(self.occupied_co[chess.WHITE] & mask))
+                    fig = chess.ROOK - 1
+                    piece_mat[color, fig, rank, file] = 1
+                elif self.queens & mask:
+                    color = int(bool(self.occupied_co[chess.WHITE] & mask))
+                    fig = chess.QUEEN - 1
+                    piece_mat[color, fig, rank, file] = 1
+                elif self.kings & mask:
+                    color = int(bool(self.occupied_co[chess.WHITE] & mask))
+                    fig = chess.KING - 1
+                    piece_mat[color, fig, rank, file] = 1
+                # check for promotion
+                if self.promoted & mask:
+                    color = int(bool(self.occupied_co[chess.WHITE] & mask))
+                    promoted_mat[color, rank, file]
+
             file  += 1
             if file >= 8:
                 file = 0
                 rank += 1
-        if flip:
-            return np.flip(np.flip(board_state, 1), 2)
-        return board_state
+
+        # Get Pockets
+        normalization = 8.0  # max number of pawns
+        pocket_mat = self.get_pockets_numpy(normalization)
+
+        # check which player is allowed to move
+        player_mat = np.zeros((2, 8, 8))
+        player_mat[int(self.turn), :, :] = 1 #Set Matrix to 1 for player that is allowed to move
+
+        # check the move count of the players
+        movecount_mat = np.full((8, 8), self.fullmove_number)
+
+        #check castling rights
+        castling_mat = np.zeros((2, 2, 8, 8))
+        # WHITE
+        # check for King Side Castling
+        if bool(self.castling_rights & chess.BB_H1) is True:
+            # White can castle with the h1 rook
+            castling_mat[chess.WHITE, 0, :, :] = 1
+        # check for Queen Side Castling
+        if bool(self.castling_rights & chess.BB_A1) is True:
+            castling_mat[chess.WHITE, 1, :, :] = 1
+
+        # BLACK
+        # check for King Side Castling
+        if bool(self.castling_rights & chess.BB_H8) is True:
+            # White can castle with the h1 rook
+            castling_mat[chess.BLACK, 0, :, :] = 1
+        # check for Queen Side Castling
+        if bool(self.castling_rights & chess.BB_A8) is True:
+            castling_mat[chess.BLACK, 1, :, :] = 1
+
+        # ToDo implment flippping
+
+        return piece_mat, pocket_mat, player_mat, movecount_mat, promoted_mat, castling_mat
+
+    def get_pockets_numpy(self, normalization = 8):
+        # oder of Pocket LEFT, RIGHT
+        # order of colors WHITE = 1, BLACK = 0
+        # White PAWN, KNIGHT, BISHOP, ROOK, QUEEN then Black PAWN, KNIGHT, BISHOP, ROOK, QUEEN
+        # 8x8 is network input size
+        ret_pockets = np.zeros((2, 5, 8, 8))
+        for pt, count in self.pockets[chess.BLACK].pieces.items():
+            ret_pockets[chess.BLACK, pt - 1, :, :] = float(count) / normalization
+        for pt, count in self.pockets[chess.WHITE].pieces.items():
+            ret_pockets[chess.WHITE, pt - 1, :, :] = float(count) / normalization
+        return ret_pockets
 
     def to_numpy_simplified(self, flip: bool):
         board_state_game = np.zeros((8, 8))
@@ -120,7 +173,6 @@ class _BughouseBoard(chess.variant.CrazyhouseBoard):
         return board_state_game
 
 
-
 class BughouseBoards:
     aliases = ["Bughouse", "Bug House", "BH"]
     uci_variant = "bughouse"
@@ -141,7 +193,7 @@ class BughouseBoards:
         for board in self.boards:
             board.reset_board()
 
-    def reset_boards(self, board_id: int):
+    def reset_board(self, board_id: int):
         self.boards[board_id].reset_board()
 
     def clear_boards(self):
@@ -180,7 +232,7 @@ class BughouseBoards:
         self.boards[self.RIGHT].pockets = copy.deepcopy(pockets_right)
 
     def get_pockets(self):
-        return copy.deepcopy(self.boards[self.LEFT].pockets), copy.deepcopy(self.boards[self.RIGHT].pockets)
+        return self.boards[self.LEFT].pockets.copy(), self.boards[self.RIGHT].pockets.copy()
 
     def board_fen(self, promoted=None):
         fen = [self.boards[0].board_fen(), self.boards[1].board_fen()]
@@ -225,28 +277,43 @@ class BughouseBoards:
 
     def to_numpy(self, flip: bool):
         # Flip to get both player on the same side
-        return np.concatenate((self.boards[0].to_numpy(False), self.boards[1].to_numpy(flip)), axis=1)
+        piece_mat_l, pocket_mat_l, player_mat_l, movecount_mat_l, promoted_mat_l, castling_mat_l =\
+            self.boards[0].to_numpy(False)
+        piece_mat_r, pocket_mat_r, player_mat_r, movecount_mat_r, promoted_mat_r, castling_mat_r = \
+            self.boards[0].to_numpy(False)
+        return np.array([piece_mat_l, piece_mat_r+1]), np.array([pocket_mat_l, pocket_mat_r+1]), \
+               np.array([player_mat_l, player_mat_r+1]), np.array([movecount_mat_l, movecount_mat_r+1]), \
+               np.array([promoted_mat_l, promoted_mat_r+1]),np.array([castling_mat_l, castling_mat_r+1])
 
     def to_numpy_simplified(self, flip: bool):
         return np.concatenate((self.boards[0].to_numpy_simplified(False), self.boards[1].to_numpy_simplified(flip)),
                               axis=1)
 
-    def get_pockets_numpy(self):
-        # oder of Pocket LEFT, RIGHT
-        # order of figurs
-        # White PAWN, KNIGHT, BISHOP, ROOK, QUEEN then Black PAWN, KNIGHT, BISHOP, ROOK, QUEEN
-        ret_pockets = np.zeros((2, 10))
-
-        for pt, count in self.boards[self.LEFT].pockets[chess.WHITE].pieces.items():
-            ret_pockets[self.LEFT, pt-1] = count
-
-        for pt, count in self.boards[self.LEFT].pockets[chess.BLACK].pieces.items():
-            ret_pockets[self.LEFT, pt+4] = count
-
-        for pt, count in self.boards[self.RIGHT].pockets[chess.WHITE].pieces.items():
-            ret_pockets[self.LEFT, pt - 1] = count
-
-        for pt, count in self.boards[self.RIGHT].pockets[chess.BLACK].pieces.items():
-            ret_pockets[self.LEFT, pt + 4] = count
-
-        return ret_pockets
+    # def get_pockets_numpy(self, create_mat:bool = True):
+    #     # oder of Pocket LEFT, RIGHT
+    #     # order of colors WHITE = 1, BLACK = 0
+    #     # White PAWN, KNIGHT, BISHOP, ROOK, QUEEN then Black PAWN, KNIGHT, BISHOP, ROOK, QUEEN
+    #     # 8x8 is network input size
+    #     normalization = 8.0 # max number of pawns
+    #     if create_mat:
+    #         ret_pockets = np.zeros((2, 2, 5, 8, 8))
+    #         for pt, count in self.boards[self.LEFT].pockets[chess.WHITE].pieces.items():
+    #             ret_pockets[self.LEFT, chess.WHITE, pt-1, :, :] = float(count) / normalization
+    #         for pt, count in self.boards[self.LEFT].pockets[chess.BLACK].pieces.items():
+    #             ret_pockets[self.LEFT, chess.BLACK, pt-1, :, :] = float(count) / normalization
+    #         for pt, count in self.boards[self.RIGHT].pockets[chess.WHITE].pieces.items():
+    #             ret_pockets[self.RIGHT, chess.WHITE, pt-1, :, :] = float(count) / normalization
+    #         for pt, count in self.boards[self.RIGHT].pockets[chess.BLACK].pieces.items():
+    #             ret_pockets[self.RIGHT, chess.BLACK, pt-1, :, :] = float(count) / normalization
+    #     else:
+    #         ret_pockets = np.zeros((2, 2, 5))
+    #         for pt, count in self.boards[self.LEFT].pockets[chess.WHITE].pieces.items():
+    #             ret_pockets[self.LEFT, chess.WHITE, pt-1] = float(count) / normalization
+    #         for pt, count in self.boards[self.LEFT].pockets[chess.BLACK].pieces.items():
+    #             ret_pockets[self.LEFT, chess.BLACK, pt-1] = float(count) / normalization
+    #         for pt, count in self.boards[self.RIGHT].pockets[chess.WHITE].pieces.items():
+    #             ret_pockets[self.RIGHT, chess.WHITE, pt-1] = float(count) / normalization
+    #         for pt, count in self.boards[self.RIGHT].pockets[chess.BLACK].pieces.items():
+    #             ret_pockets[self.RIGHT, chess.BLACK, pt-1] = float(count) / normalization
+    #
+    #     return ret_pockets
