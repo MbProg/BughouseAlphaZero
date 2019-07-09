c3cada4b6fd862cc719b71f82dd8fb2a565e80ee
MSI\MoBray <mbmain2013@gmail.com>
2019-07-09 14:58:54 +0200

    Make NN to work in Multithreading: https://github.com/tensorflow/tensorflow/issues/14356

--- bughouse/keras/BugHouseNet.py
+++ bughouse/keras/BugHouseNet.py
@@ -10,18 +10,57 @@ from keras import optimizers
 from keras.callbacks import ModelCheckpoint
 from keras.layers import *
 import keras
+import tensorflow as tf
 
+def acc_sign(y_true, y_pred):
+    return K.mean(K.equal(K.sign(y_pred), K.sign(y_true)), axis=-1)
+
+
+def acc_round(y_true, y_pred):
+    # each interval for class -1, 0, 1 must have a width of 2/3 to be equally distributed
+    # therefore you must convert (1/3) to (1/2) and only then round to the closest integer
+    # 1/3 * x = 1/2
+    return K.mean(K.equal(K.round(y_pred * 1.5), K.round(y_true)), axis=-1)
+
+def acc_reg(y_true,y_pred):
+    return K.constant(1) - K.square(K.mean((y_pred-y_true), axis=1))
+
+def acc_round_unequal(y_true,y_pred):
+    return K.mean(K.equal(K.round(y_true),K.round(y_pred)), axis=-1)
+graph = None
 
 class BugHouseNet():
     def __init__(self, args, modelweights_path='models/model-05.hdf5'):
+
         self.CLASSES_LEN = NB_LABELS
         self.channel_pos = 'channels_last'
         self.inp_shape = (60, 8, 8)  # TODO: this should be read from the environment
+
         self.model = self.__getResidualNetwork(self.inp_shape, output_policy=self.CLASSES_LEN)
         sgd = optimizers.SGD(lr=0.000, momentum=0.9, decay=0.0, nesterov=False)
-        self.model.compile(loss=['categorical_crossentropy', 'mean_squared_error'], optimizer=sgd,
-                           metrics=['accuracy'], loss_weights=[0.999, 0.001])
-        self.model.load_weights(modelweights_path)
+
+        self.model.compile(loss={'policy':'categorical_crossentropy',
+                            'value':'mean_squared_error'}, optimizer=sgd,
+                    metrics={'policy':'accuracy', 'value':[acc_round, acc_sign]}, loss_weights=[0.25,0.75])
+
+        self.model.load_weights('finalModel/model-05.hdf5')
+
+        global graph
+        graph = tf.get_default_graph()
+        # dataTest = np.load('data.npy')
+        # p,v = self.model.predict(dataTest)
+        # print(p,v)
+        # dataset_path = 'dataset/'                     # relative directory to the dataset
+
+        # x_val, policies_val, values_val = read_files_data([6001],path=dataset_path)
+        # self.model.evaluate(x_val,[policies_val,values_val],verbose=True)
+
+        # print('success')
+        # self.model = self.__getResidualNetwork(self.inp_shape, output_policy=self.CLASSES_LEN)
+        # sgd = optimizers.SGD(lr=0.000, momentum=0.9, decay=0.0, nesterov=False)
+        # self.model.compile(loss=['categorical_crossentropy', 'mean_squared_error'], optimizer=sgd,
+        #                    metrics=['accuracy'], loss_weights=[0.999, 0.001])
+        # self.model.load_weights(modelweights_path)
 
     def __VGG_Conv2DBlock(self, depth, kernelshape, activation, padding, channel_pos, x, conv_amount=3, inp_shape=None):
         ''' 
@@ -104,7 +143,12 @@ class BugHouseNet():
         Input: numpy array with shape (1, shape of state) e.g. (1, 60, 8 , 8)
                 should be channel_first
         """
-        pi, v = self.model.predict(data)
+        data = data[0][np.newaxis, :, :]
+        random_data = np.random.rand(1,60,8,8)
+        # data = np.float32(data)
+        with graph.as_default():
+            pi, v = self.model.predict(data)      
+        # pi, v = self.model.predict(data)
 
         # print('PREDICTION TIME TAKEN : {0:03f}'.format(time.time()-start))
         return pi[0], v[0]
